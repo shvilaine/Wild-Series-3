73dffcec70ee1cab00df3baf049a679e2d2f6ec8
Severine Helene Vilaine <severine.vilaine@gmail.com>
2022-11-28 23:58:39 +0100

    Challenge for Symfony 5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program/', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render(
@@ -17,5 +18,21 @@ class ProgramController extends AbstractController
                 'website' => 'Wild Series',
             ]
         );
+    }
+
+    #[Route('/{id}', methods: ['GET'], requirements: ['id' => '\d+'], name: 'show')]
+    public function show(): Response
+    {
+        return $this->render(
+            'program/show.html.twig',
+            [
+                'id' => 'id',
+            ]
+        );
+    }
+
+    public function new(): Response
+    {
+        return $this->redirectToRoute('program_show', ['id' => 4]);
     }
 }
