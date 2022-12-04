277d56d798c6281c9e62941f7b65dd54dc0e1078
Severine Helene Vilaine <severine.vilaine@gmail.com>
2022-12-05 00:46:22 +0100

    Site now with forms!

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
-use App\Repository\CategoryRepository;
-use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
+use App\Repository\ProgramRepository;
+use App\Form\CategoryType;
+use App\Entity\Category;
 
 #[Route('/category', name: 'category_')]
 class CategoryController extends AbstractController
@@ -23,14 +26,44 @@ class CategoryController extends AbstractController
         );
     }
 
+    #[Route('/new', name: 'new')]
+    public function new(Request $request, CategoryRepository $categoryRepository): Response
+    {
+        $category = new Category();
+
+        // Create the form, linked with $category
+        $form = $this->createForm(CategoryType::class, $category);
+
+        // Get data from HTTP request
+        $form->handleRequest($request);
+
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            $categoryRepository->save($category, true);
+
+            // Redirect to categories list
+            return $this->redirectToRoute('category_index');
+        }
+
+        // Render the form (best practice)
+        return $this->renderForm('category/new.html.twig', [
+            'form' => $form,
+        ]);
+
+        // Alternative
+        // return $this->render('category/new.html.twig', [
+        //   'form' => $form->createView(),
+        // ]);
+    }
+
     #[Route('/{categoryName}', methods: ['GET'], name: 'show')]
     public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository): Response
     {
         $category = $categoryRepository->findOneBy(['name' => $categoryName]);
 
-        if(!$category) {
+        if (!$category) {
             throw $this->createNotFoundException(
-                'No category with the name: '.$categoryName.' found in the table.'
+                'No category with the name: ' . $categoryName . ' found in the table.'
             );
         }
 
